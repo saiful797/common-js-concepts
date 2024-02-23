0f34d4359512bd46073aa8392bd7bb39ebd6d807
saiful797 <saifulislamsaif797@gmail.com>
2024-02-23 20:37:01 +0600

    Updated triple.js

---
+++ triple.js
@@ -0,0 +1,11 @@
+//always use ===, check vale same or not and type .
+// equal comparison doesn't  work for non-primitive
+
+const a = 3;
+const b = 3;
+if(a === b){
+    console.log('They are equal.');
+}
+else {
+    console.log('They are not equal.');
+}
