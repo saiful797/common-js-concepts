59f13383b9c5bfb9d6e28771189e3f797f4849ff
saiful797 <saifulislamsaif797@gmail.com>
2024-02-23 18:51:15 +0600

    first commit

---
+++ types.js
@@ -0,0 +1,12 @@
+//JavaScript is a dynamic typed programming language
+
+//primitive type
+const a = 13;
+const b = 'Aso khelte jai';
+const c = true;
+
+//
+const array = [2, 3, 4, 5, 6];
+const student = {name: 'Asik', id: 5};
+
+console.log(typeof a, typeof b, typeof c, typeof array, typeof student );
