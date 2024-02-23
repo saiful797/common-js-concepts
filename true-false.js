ffc4fb4eed4865aaa8b2f3214664b3a73ab299e1
saiful797 <saifulislamsaif797@gmail.com>
2024-02-23 20:13:48 +0600

    added true-false.js

---
+++ true-false.js
@@ -0,0 +1,25 @@
+ /*
+Truthy:
+1. true
+2. any number (+ve, -ve) will be return true
+3. any string other than empty string
+4. '0', 'false'
+5. {}
+6. 
+
+Falsy:
+1. false
+2. 0
+3. "" (empty string)
+4. null
+5. undefined
+*/
+
+  const x = false;
+  if(x){
+    console.log('value of x is truthy');
+  }
+
+  else{
+    console.log('value of x is falsy');
+  }
